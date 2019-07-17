918dba97105b91e911a78f6fa7654b7427d5b6cf
Vivia Xia <676494430@qq.com>
2019-07-17 23:35:21 +0800

    add new myinfo page

---
+++ pages/myinfo/myinfo.vue
@@ -0,0 +1,162 @@
+<template>
+  <view class="content">
+    
+    <view v-if="hasLogin" class="hello">
+      <view class="title">您好 {{userName}}，您已成功登录。</view>
+      <view class="ul">
+        <view>这是 uni-app 带登录模板的示例App首页。</view>
+        <view>在 “我的” 中点击 “退出” 可以 “注销当前账户”</view>
+      </view>
+    </view>
+    <view v-if="!hasLogin" class="hello">
+      <view class="title">您好 游客。</view>
+      <view class="ul">
+        <view>这是 uni-app 带登录模板的示例App首页。</view>
+        <view>在 “我的” 中点击 “登录” 可以 “登录您的账户”</view>
+      </view>
+      <button type="primary" class="primary" @tap="testAPI">testAPI</button>
+      <import src="../createpost/index.wxml"/>
+      <view is="uploadimg"/>
+    </view>
+
+  </view>
+	
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: mapState(["forcedLogin", "hasLogin", "userName"]),
+  onLoad() {
+    if (!this.hasLogin) {
+      uni.showModal({
+        title: "未登录",
+        content: "您未登录，需要登录后才能继续",
+        /**
+         * 如果需要强制登录，不显示取消按钮
+         */
+        showCancel: !this.forcedLogin,
+        success: res => {
+          if (res.confirm) {
+            /**
+             * 如果需要强制登录，使用reLaunch方式
+             */
+            if (this.forcedLogin) {
+              uni.reLaunch({
+                url: "../login/login"
+              });
+            } else {
+              uni.navigateTo({
+                url: "../login/login"
+              });
+            }
+          }
+        }
+      });
+    }
+  },
+
+  methods: {
+    testAPI() {
+      uni.request({
+        url: this.$servertestUrl + "/api/packages/",
+        success: res => {
+          console.log("老婆是豬");
+          if (res.data.code !== 0) {
+            return;
+          }
+
+          this.data = this.data.concat(res.data.data);
+        },
+        fail: () => {
+          console.log("e");
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.hello {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+}
+
+.title {
+  color: #8f8f94;
+  margin-top: 50upx;
+}
+
+.ul {
+  font-size: 30upx;
+  color: #8f8f94;
+  margin-top: 50upx;
+}
+
+.ul > view {
+  line-height: 50upx;
+}
+.pic-title {
+  margin: 20rpx;
+  font-size: 30rpx;
+  font-weight: 500;
+  color: rgba(34, 34, 34, 1);
+}
+
+.pic-title .pic-title-content-right {
+  color: #999;
+}
+
+.pic-title .pic-title-content-right-count {
+  color: #fb0000;
+}
+
+.pic-content {
+  margin-left: 10rpx;
+  margin-right: 10rpx;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 60rpx;
+  margin-bottom: 60rpx;
+}
+
+.pic-content .camera {
+  width: 175rpx;
+  height: 175rpx;
+  background: rgba(255, 255, 255, 1);
+  border-radius: 10rpx;
+  position: relative;
+  margin-top: 20rpx;
+}
+
+.pic-content .camera .img {
+  width: 74rpx;
+  height: 54rpx;
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -55%);
+}
+
+.pic-content .each-img {
+  overflow: visible;
+  margin-left: 10rpx;
+  width: 175rpx;
+  height: 175rpx;
+  margin-top: 20rpx;
+  position: relative;
+}
+
+.pic-content .each-img .delete-icon {
+  background-color: white;
+  border-radius: 50%;
+  position: absolute;
+  right: 5rpx;
+  top: 0rpx;
+  width: 38rpx;
+  height: 38rpx;
+}
+</style>
--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,44 +1,140 @@
 <template>
-    <view class="content">
-        <view class="btn-row">
-            <button v-if="!hasLogin" type="primary" class="primary" @tap="bindLogin">登录</button>
-            <button v-if="hasLogin" type="default" @tap="bindLogout">退出登录</button>
-        </view>
+  <view class="content">
+    <view class="btn-row">
+      <button v-if="!hasLogin" type="primary" class="primary" @tap="bindLogin">登录</button>
     </view>
+    <view v-if="hasLogin" class="hr"></view>
+    <view v-if="hasLogin" class="item">
+      <view class="title" @click="navmyinfo">个人信息</view>
+      <view class="detail2">
+        <text>></text>
+      </view>
+    </view>
+    <view v-if="hasLogin" class="line"></view>
+    <view v-if="hasLogin" class="hr"></view>
+    <view v-if="hasLogin" class="item">
+      <view class="title">我的帖子</view>
+      <view class="detail2">
+        <text>></text>
+      </view>
+    </view>
+    <view v-if="hasLogin" class="line"></view>
+    <view v-if="hasLogin" class="hr"></view>
+    <view v-if="hasLogin" class="item">
+      <view class="title">我的互寄</view>
+      <view class="detail2">
+        <text>></text>
+      </view>
+    </view>
+    <view v-if="hasLogin" class="line"></view>
+    <view v-if="hasLogin" class="hr"></view>
+    <view v-if="hasLogin" class="item">
+      <view class="title">我的互寄</view>
+      <view class="detail2">
+        <text>></text>
+      </view>
+    </view>
+    <view v-if="hasLogin" class="line"></view>
+    <button v-if="hasLogin" type="default" @tap="bindLogout">退出登录</button>
+  </view>
 </template>
 
 <script>
-    import {
-        mapState,
-        mapMutations
-    } from 'vuex'
+import { mapState, mapMutations } from "vuex";
 
-    export default {
-        computed: {
-            ...mapState(['hasLogin', 'forcedLogin'])
-        },
-        methods: {
-            ...mapMutations(['logout']),
-            bindLogin() {
-                uni.navigateTo({
-                    url: '../login/login',
-                });
-            },
-            bindLogout() {
-                this.logout();
-                /**
-                 * 如果需要强制登录跳转回登录页面
-                 */
-                if (this.forcedLogin) {
-                    uni.reLaunch({
-                        url: '../login/login',
-                    });
-                }
-            }
-        }
+export default {
+  computed: {
+    ...mapState(["hasLogin", "forcedLogin"])
+  },
+  methods: {
+    ...mapMutations(["logout"]),
+    bindLogin() {
+      uni.navigateTo({
+        url: "../login/login"
+      });
+    },
+    navmyinfo() {
+      uni.navigateTo({
+        url: "../myinfo/myinfo"
+      });
+    },
+    bindLogout() {
+      this.logout();
+      /**
+       * 如果需要强制登录跳转回登录页面
+       */
+      if (this.forcedLogin) {
+        uni.reLaunch({
+          url: "../login/login"
+        });
+      }
     }
+  }
+};
 </script>
 
 <style>
+.bg {
+  height: 150px;
+  background-color: #d53e37;
+}
 
+.head {
+  display: flex;
+  flex-direction: column;
+}
+
+.headIcon {
+  display: flex;
+  justify-content: center;
+}
+
+.login {
+  display: flex;
+  color: #fff;
+  font-size: 15px;
+  margin-top: 15rpx;
+  justify-content: center;
+}
+
+.button {
+  margin: 10px;
+  font-size: 14px;
+}
+
+.head image {
+  border-radius: 50%;
+}
+
+.hr {
+  width: 100%;
+  height: 15px;
+  background-color: #f4f5f6;
+}
+
+.item {
+  display: flex;
+  flex-direction: row;
+}
+
+.title {
+  padding-top: 15px;
+  padding-bottom: 15px;
+  padding-left: 15px;
+  font-size: 15px;
+}
+
+.detail2 {
+  font-size: 15px;
+  position: absolute;
+  right: 10px;
+  height: 50px;
+  line-height: 50px;
+  color: #888;
+}
+
+.line {
+  border: 1px solid #ccc;
+  opacity: 0.2;
+}
 </style>
